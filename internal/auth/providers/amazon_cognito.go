82fc18abac657d3d3843916a99d6bd7496758afd
Justin Hines <justinphines@gmail.com>
2019-08-29 09:18:54 -0400

    amazon-cognito: init

---
+++ internal/auth/providers/amazon_cognito.go
@@ -0,0 +1,381 @@
+package providers
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/buzzfeed/sso/internal/auth/circuit"
+	log "github.com/buzzfeed/sso/internal/pkg/logging"
+	"github.com/buzzfeed/sso/internal/pkg/sessions"
+	"github.com/datadog/datadog-go/statsd"
+)
+
+// AmazonCognitoProvider is an implementation of the Provider interface.
+type AmazonCognitoProvider struct {
+	*ProviderData
+	StatsdClient *statsd.Client
+	cb           *circuit.Breaker
+}
+
+// NewAmazonCognitoProvider returns a new AmazonCognitoProvider and sets the provider url endpoints.
+func NewAmazonCognitoProvider(p *ProviderData, OrgURL string) (*AmazonCognitoProvider, error) {
+	if OrgURL == "" {
+		return nil, errors.New("missing setting: amazon_cognito_url")
+	}
+
+	p.ProviderName = "AmazonCognito"
+	scheme := "https"
+
+	// interact with resource owner and obtain an authorization grant
+	p.SignInURL = &url.URL{
+		Scheme: scheme,
+		Host:   OrgURL,
+		Path:   "/oauth2/authorize",
+	}
+
+	// returns access tokens, ID tokens or refresh tokens
+	p.RedeemURL = &url.URL{
+		Scheme: scheme,
+		Host:   OrgURL,
+		Path:   "/oauth2/token",
+	}
+
+	// TODO: Cognito doesn't provide a revoke endpoint to log out users
+	// revokes an access or refresh token
+	p.RevokeURL = &url.URL{}
+
+	// takes an access token and returns claims for the currently logged-in user
+	p.ProfileURL = &url.URL{
+		Scheme: scheme,
+		Host:   OrgURL,
+		Path:   "/oauth2/userinfo",
+	}
+
+	// TODO: This isn't a validate endpoint but repeating userinfo
+	// takes token as a URL query and returns active/inactive, and further info if active.
+	p.ValidateURL = &url.URL{
+		Scheme: scheme,
+		Host:   OrgURL,
+		Path:   "/oauth2/userinfo",
+	}
+
+	if p.Scope == "" {
+		p.Scope = "openid profile email groups"
+	}
+
+	amazonCognitoProvider := &AmazonCognitoProvider{
+		ProviderData: p,
+	}
+
+	amazonCognitoProvider.cb = circuit.NewBreaker(&circuit.Options{
+		HalfOpenConcurrentRequests: 2,
+		OnStateChange:              amazonCognitoProvider.cbStateChange,
+		OnBackoff:                  amazonCognitoProvider.cbBackoff,
+		ShouldTripFunc:             func(c circuit.Counts) bool { return c.ConsecutiveFailures >= 3 },
+		ShouldResetFunc:            func(c circuit.Counts) bool { return c.ConsecutiveSuccesses >= 6 },
+		BackoffDurationFunc: circuit.ExponentialBackoffDuration(
+			time.Duration(200)*time.Second, time.Duration(500)*time.Millisecond,
+		),
+	})
+
+	return amazonCognitoProvider, nil
+}
+
+// Sets the providers StatsdClient
+func (p *AmazonCognitoProvider) SetStatsdClient(statsdClient *statsd.Client) {
+	p.StatsdClient = statsdClient
+}
+
+// ValidateSessionState attempts to validate the session state's access token.
+func (p *AmazonCognitoProvider) ValidateSessionState(s *sessions.SessionState) bool {
+	if s.AccessToken == "" {
+		return false
+	}
+
+	_, err := p.GetUserProfile(s.AccessToken)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// GetSignInURL returns the sign in url with typical oauth parameters
+func (p *AmazonCognitoProvider) GetSignInURL(redirectURI, state string) string {
+	// https://developer.okta.com/docs/api/resources/oidc/#authorize
+	var a url.URL
+	a = *p.SignInURL
+
+	params, _ := url.ParseQuery(a.RawQuery)
+	params.Set("response_type", "code")
+	params.Set("client_id", p.ClientID)
+	params.Set("redirect_uri", redirectURI)
+	params.Add("state", state)
+	params.Set("identity_provider", "COGNITO")
+	params.Add("scope", p.Scope)
+
+	a.RawQuery = params.Encode()
+	return a.String()
+}
+
+func (p *AmazonCognitoProvider) amazonCognitoRequest(method, endpoint string, params url.Values, tags []string, header http.Header, response interface{}) error {
+	logger := log.NewLogEntry()
+
+	startTS := time.Now()
+	var body io.Reader
+
+	tags = append(tags, "provider:amazonCognito")
+	switch method {
+	case "POST":
+		body = bytes.NewBufferString(params.Encode())
+	case "GET":
+		// error checking skipped because we are just parsing in
+		// order to make a copy of an existing URL
+		u, _ := url.Parse(endpoint)
+		u.RawQuery = params.Encode()
+		endpoint = u.String()
+	default:
+		// ¯\_(ツ)_/¯
+		return ErrBadRequest
+	}
+
+	req, err := http.NewRequest(method, endpoint, body)
+	if err != nil {
+		return err
+	}
+	if header != nil {
+		req.Header = header
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	p.StatsdClient.Incr("provider.request", tags, 1.0)
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		tags = append(tags, "error:http_client_error")
+		p.StatsdClient.Incr("provider.internal_error", tags, 1.0)
+		return err
+	}
+	tags = append(tags, fmt.Sprintf("status_code:%d", resp.StatusCode))
+	p.StatsdClient.Timing("provider.latency", time.Now().Sub(startTS), tags, 1.0)
+	p.StatsdClient.Incr("provider.response", tags, 1.0)
+
+	var respBody []byte
+	respBody, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		tags = append(tags, "error:invalid_body")
+		p.StatsdClient.Incr("provider.internal_error", tags, 1.0)
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		p.StatsdClient.Incr("provider.error", tags, 1.0)
+		logger.WithHTTPStatus(resp.StatusCode).WithEndpoint(stripToken(endpoint)).WithResponseBody(
+			respBody).Info()
+		switch resp.StatusCode {
+		case 400:
+			var response struct {
+				Error            string `json:"error"`
+				ErrorDescription string `json:"error_description"`
+			}
+			e := json.Unmarshal(respBody, &response)
+			if e == nil && response.ErrorDescription == "Token expired or revoked" {
+				p.StatsdClient.Incr("provider.token_revoked", tags, 1.0)
+				return ErrTokenRevoked
+			}
+			return ErrBadRequest
+		case 429:
+			return ErrRateLimitExceeded
+		default:
+			return ErrServiceUnavailable
+		}
+	}
+
+	if response != nil {
+		err := json.Unmarshal(respBody, &response)
+		if err != nil {
+			p.StatsdClient.Incr("provider.internal_error", tags, 1.0)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *AmazonCognitoProvider) cbBackoff(duration time.Duration, reset time.Time) {
+	logger := log.NewLogEntry()
+
+	p.StatsdClient.Timing("provider.backoff", duration, nil, 1.0)
+	logger.WithBackoffDuration(duration).WithBackoffReset(reset).Info(
+		"circuit breaker backoff set")
+}
+
+func (p *AmazonCognitoProvider) cbStateChange(from, to circuit.State) {
+	logger := log.NewLogEntry()
+
+	p.StatsdClient.Incr("provider.circuit_change", []string{fmt.Sprintf("from_state:%s", from), fmt.Sprintf("to_state:%s", to)}, 1.0)
+	logger.WithCircuitChangeFrom(from).WithCircuitChangeTo(to).Info("circuit breaker trigger")
+}
+
+// Redeem fulfills the Provider interface.
+// The authenticator uses this method to redeem the code provided to /callback after the user logs into their AmazonCognito account.
+// The code is redeemed for an access token and refresh token
+// 1. POSTs the code and grant_type to https://docs.aws.amazon.com/cognito/latest/developerguide/token-endpoint.html
+// 2. If the request fails, the authenticator will return a 500 and display an error page (see oauth_proxy.go#OAuthCallback)
+// 3. If the request succeeds, the data from AmazonCognito contains:
+//     - the access token which we use to get data from AmazonCognito
+//     - the refresh token which we can use to get a new access_token
+//     - the expiration time of the access token
+//     - a Base64 encoded id token which contains the user's email
+//       address and whether or not that email address is verified
+func (p *AmazonCognitoProvider) Redeem(redirectURL, code string) (*sessions.SessionState, error) {
+	if code == "" {
+		return nil, ErrBadRequest
+	}
+	params := url.Values{}
+	params.Add("redirect_uri", redirectURL)
+	params.Add("client_id", p.ClientID)
+	params.Add("code", code)
+	params.Add("grant_type", "authorization_code")
+	params.Add("scope", p.Scope)
+
+	var response struct {
+		AccessToken  string `json:"access_token"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresIn    int64  `json:"expires_in"`
+		IDToken      string `json:"id_token"`
+	}
+
+	b64auth := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", p.ClientID, p.ClientSecret)))
+	header := http.Header{}
+	header.Set("Authorization", fmt.Sprintf("Basic %s", b64auth))
+
+	err := p.amazonCognitoRequest("POST", p.RedeemURL.String(), params, []string{"action:redeem"}, nil, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	email, err := p.verifyEmailWithAccessToken(response.AccessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return &sessions.SessionState{
+		AccessToken:  response.AccessToken,
+		RefreshToken: response.RefreshToken,
+
+		RefreshDeadline:  sessions.ExtendDeadline(time.Duration(response.ExpiresIn) * time.Second),
+		LifetimeDeadline: sessions.ExtendDeadline(p.SessionLifetimeTTL),
+		Email:            email,
+	}, nil
+}
+
+// verifyEmailWithAccessToken takes in an access token and
+// checks if it is verified with AmazonCognito. Conditionally returns
+// an empty string, or if validated the email address in question.
+func (p *AmazonCognitoProvider) verifyEmailWithAccessToken(accessToken string) (string, error) {
+	if accessToken == "" {
+		return "", ErrBadRequest
+	}
+
+	userinfo, err := p.GetUserProfile(accessToken)
+	if err != nil {
+		return "", err
+	}
+
+	if userinfo.EmailAddress == "" {
+		return "", errors.New("missing email")
+	}
+
+	return userinfo.EmailAddress, nil
+}
+
+// ValidateGroupMembership takes in an email, a list of allowed groups an access token
+// TODO
+// - compares allowed groups to users' groups.
+// Conditionally returns an empty (nil) slice or a slice of the matching groups.
+func (p *AmazonCognitoProvider) ValidateGroupMembership(email string, allowedGroups []string, accessToken string) ([]string, error) {
+	return allowedGroups, nil
+}
+
+// GetUserProfile takes in an access token and sends a request to the /userinfo endpoint.
+// Conditionally returns nil response or a struct of specified claims.
+func (p *AmazonCognitoProvider) GetUserProfile(AccessToken string) (*GetUserProfileResponse, error) {
+	response := &GetUserProfileResponse{}
+
+	if AccessToken == "" {
+		return nil, ErrBadRequest
+	}
+
+	bearer := fmt.Sprintf("Bearer %s", AccessToken)
+	header := http.Header{}
+	header.Set("Authorization", bearer)
+
+	err := p.amazonCognitoRequest("GET", p.ProfileURL.String(), nil, []string{"action:userinfo"}, header, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// RefreshSessionIfNeeded takes in a SessionState and
+// returns false if the session is not refreshed and true if it is.
+func (p *AmazonCognitoProvider) RefreshSessionIfNeeded(s *sessions.SessionState) (bool, error) {
+	if s == nil || !s.RefreshPeriodExpired() || s.RefreshToken == "" {
+		return false, nil
+	}
+
+	newToken, duration, err := p.RefreshAccessToken(s.RefreshToken)
+	if err != nil {
+		return false, err
+	}
+	logger := log.NewLogEntry()
+
+	s.AccessToken = newToken
+
+	s.RefreshDeadline = time.Now().Add(duration).Truncate(time.Second)
+	logger.WithUser(s.Email).WithRefreshDeadline(s.RefreshDeadline).Info("refreshed access token")
+
+	return true, nil
+}
+
+// RefreshAccessToken takes in a refresh token and returns the new access token along with an expiration date.
+func (p *AmazonCognitoProvider) RefreshAccessToken(refreshToken string) (token string, expires time.Duration, err error) {
+	params := url.Values{}
+	params.Add("client_id", p.ClientID)
+	params.Add("refresh_token", refreshToken)
+	params.Add("grant_type", "refresh_token")
+
+	var response struct {
+		AccessToken string `json:"access_token"`
+		ExpiresIn   int64  `json:"expires_in"`
+	}
+
+	b64auth := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", p.ClientID, p.ClientSecret)))
+	header := http.Header{}
+	header.Set("Authorization", fmt.Sprintf("Basic %s", b64auth))
+
+	err = p.amazonCognitoRequest("POST", p.RedeemURL.String(), params, []string{"action:refresh"}, header, &response)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return response.AccessToken, time.Duration(response.ExpiresIn) * time.Second, nil
+}
+
+// Revoke revokes the refresh token from a given session state.
+// For the Amazon Cognito provider this triggers a global sign out
+// Revoking the refresh token implicitly revokes the access token, forcing re-authentication.
+func (p *AmazonCognitoProvider) Revoke(s *sessions.SessionState) error {
+	// TODO: There is no revoke endpoint
+	// Global Sign Out?
+	return nil
+}
---
+++ internal/auth/providers/amazon_cognito_test.go
@@ -0,0 +1,267 @@
+package providers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/buzzfeed/sso/internal/pkg/sessions"
+	"github.com/buzzfeed/sso/internal/pkg/testutil"
+)
+
+func newAmazonCognitoProviderServer(body []byte, code int) (*url.URL, *httptest.Server) {
+	s := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(code)
+		rw.Write(body)
+	}))
+	u, _ := url.Parse(s.URL)
+	return u, s
+}
+func newAmazonCognitoProvider(providerData *ProviderData, t *testing.T) *AmazonCognitoProvider {
+	if providerData == nil {
+		providerData = &ProviderData{
+			ProviderName: "",
+			ClientID:     "",
+			ClientSecret: "",
+			SignInURL:    &url.URL{},
+			RedeemURL:    &url.URL{},
+			RevokeURL:    &url.URL{},
+			ProfileURL:   &url.URL{},
+			ValidateURL:  &url.URL{},
+			Scope:        ""}
+	}
+	provider, err := NewAmazonCognitoProvider(providerData, "test.amazonCognito.com")
+	if err != nil {
+		t.Fatalf("new amazonCognito provider returns unexpected error: %q", err)
+	}
+	return provider
+}
+
+func TestAmazonCognitoProviderDefaults(t *testing.T) {
+	expectedResults := []struct {
+		name         string
+		providerData *ProviderData
+		signInURL    string
+		redeemURL    string
+		revokeURL    string
+		profileURL   string
+		userInfoURL  string
+		validateURL  string
+		scope        string
+	}{
+		{
+			name:        "defaults",
+			signInURL:   "https://test.amazonCognito.com/oauth2/authorize",
+			redeemURL:   "https://test.amazonCognito.com/oauth2/token",
+			revokeURL:   "", // TODO
+			profileURL:  "https://test.amazonCognito.com/oauth2/userinfo",
+			validateURL: "https://test.amazonCognito.com/oauth2/userinfo",
+			scope:       "openid profile email groups",
+		},
+	}
+	for _, expected := range expectedResults {
+		t.Run(expected.name, func(t *testing.T) {
+			p := newAmazonCognitoProvider(expected.providerData, t)
+			if p == nil {
+				t.Errorf("AmazonCognito provider was nil")
+			}
+			if p.Data().ProviderName != "AmazonCognito" {
+				t.Errorf("expected provider name AmazonCognito, got %q", p.Data().ProviderName)
+			}
+			if p.Data().SignInURL.String() != expected.signInURL {
+				t.Logf("expected %q", expected.signInURL)
+				t.Logf("     got %q", p.Data().SignInURL.String())
+				t.Errorf("unexpected signin url")
+			}
+			if p.Data().RedeemURL.String() != expected.redeemURL {
+				t.Logf("expected %q", expected.redeemURL)
+				t.Logf("     got %q", p.Data().RedeemURL.String())
+				t.Errorf("unexpected redeem url")
+			}
+
+			if p.Data().RevokeURL.String() != expected.revokeURL {
+				t.Logf("expected %q", expected.revokeURL)
+				t.Logf("     got %q", p.Data().RevokeURL.String())
+				t.Errorf("unexpected revoke url")
+			}
+
+			if p.Data().ValidateURL.String() != expected.validateURL {
+				t.Logf("expected %q", expected.validateURL)
+				t.Logf("     got %q", p.Data().ValidateURL.String())
+				t.Errorf("unexpected validate url")
+			}
+
+			if p.Data().ProfileURL.String() != expected.profileURL {
+				t.Logf("expected %q", expected.profileURL)
+				t.Logf("     got %q", p.Data().ProfileURL.String())
+				t.Errorf("unexpected profile url")
+			}
+
+			if p.Data().Scope != expected.scope {
+				t.Logf("expected %q", expected.scope)
+				t.Logf("     got %q", p.Data().Scope)
+				t.Errorf("unexpected scope")
+			}
+		})
+
+	}
+}
+
+type amazonCognitoProviderRedeemResponse struct {
+	AccessToken   string `json:"access_token"`
+	RefreshToken  string `json:"refresh_token"`
+	EmailAddress  string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	ExpiresIn     int64  `json:"expires_in"`
+}
+
+func TestAmazonCognitoProviderRedeem(t *testing.T) {
+	testCases := []struct {
+		name            string
+		resp            amazonCognitoProviderRedeemResponse
+		expectedError   bool
+		expectedSession *sessions.SessionState
+	}{
+		{
+			name: "redeem",
+			resp: amazonCognitoProviderRedeemResponse{
+				AccessToken:   "a1234",
+				EmailAddress:  "michael.bland@gsa.gov",
+				EmailVerified: true,
+				ExpiresIn:     10,
+				RefreshToken:  "refresh12345",
+			},
+			expectedSession: &sessions.SessionState{
+				Email:        "michael.bland@gsa.gov",
+				AccessToken:  "a1234",
+				RefreshToken: "refresh12345",
+			},
+		},
+		{
+			name: "missing email",
+			resp: amazonCognitoProviderRedeemResponse{
+				AccessToken: "a1234",
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			p := newAmazonCognitoProvider(nil, t)
+			body, err := json.Marshal(tc.resp)
+			testutil.Equal(t, nil, err)
+			var server *httptest.Server
+			p.RedeemURL, server = newAmazonCognitoProviderServer(body, http.StatusOK)
+			p.ProfileURL, server = newAmazonCognitoProviderServer(body, http.StatusOK)
+			defer server.Close()
+
+			session, err := p.Redeem("http://redirect/", "code1234")
+			if tc.expectedError && err == nil {
+				t.Errorf("expected redeem error but was nil")
+			}
+			if !tc.expectedError && err != nil {
+				t.Errorf("unexpected error %q", err)
+			}
+			if tc.expectedSession == nil && session != nil {
+				t.Errorf("expected session to be nil but it was %q", session)
+			}
+			if session != nil && tc.expectedSession != nil {
+				if session.Email != tc.expectedSession.Email {
+					t.Logf("expected email %q", tc.expectedSession.Email)
+					t.Logf("           got %q", session.Email)
+					t.Errorf("unexpected session email")
+				}
+
+				if session.AccessToken != tc.expectedSession.AccessToken {
+					t.Logf("expected access token %q", tc.expectedSession.AccessToken)
+					t.Logf("                  got %q", session.AccessToken)
+					t.Errorf("unexpected access token")
+				}
+
+				if session.RefreshToken != tc.expectedSession.RefreshToken {
+					t.Logf("expected refresh token %q", tc.expectedSession.RefreshToken)
+					t.Logf("                   got %q", session.RefreshToken)
+					t.Errorf("unexpected session refresh token")
+				}
+			}
+		})
+	}
+}
+
+type amazonCognitoProviderRevokeErrorResponse struct {
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+}
+
+func TestAmazonCognitoProviderRevoke(t *testing.T) {
+	testCases := []struct {
+		name                string
+		resp                amazonCognitoProviderRevokeErrorResponse
+		httpStatus          int
+		sessionState        *sessions.SessionState
+		expectedError       bool
+		expectedErrorString string
+	}{
+		{
+			name: "idempotent revoke",
+			resp: amazonCognitoProviderRevokeErrorResponse{
+				Error:            "invalid_token",
+				ErrorDescription: "Token expired or revoked",
+			},
+			sessionState: &sessions.SessionState{
+				AccessToken:     "access1234",
+				RefreshDeadline: time.Now(),
+				RefreshToken:    "refresh1234",
+				Email:           "logged.out@example.com",
+			},
+			httpStatus: http.StatusBadRequest,
+		},
+		{
+			name: "can still fail",
+			resp: amazonCognitoProviderRevokeErrorResponse{
+				Error:            "not_invalid_token",
+				ErrorDescription: "Something else happened internally",
+			},
+			sessionState: &sessions.SessionState{
+				AccessToken:     "access1234",
+				RefreshDeadline: time.Now(),
+				RefreshToken:    "refresh1234",
+				Email:           "logged.out@example.com",
+			},
+			httpStatus:          http.StatusForbidden,
+			expectedErrorString: "SERVICE_UNAVAILABLE",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			p := newAmazonCognitoProvider(nil, t)
+			body, err := json.Marshal(tc.resp)
+			testutil.Equal(t, nil, err)
+			var server *httptest.Server
+			p.RevokeURL, server = newAmazonCognitoProviderServer(body, tc.httpStatus)
+			defer server.Close()
+
+			err = p.Revoke(tc.sessionState)
+			if tc.expectedError && err != nil {
+				t.Errorf("unexpected error %q", err)
+			}
+			if tc.expectedError {
+				if err == nil {
+					t.Errorf("expected error but err was nil")
+				}
+				if !strings.Contains(err.Error(), tc.expectedErrorString) {
+					t.Logf("expected error string to contain %q", tc.expectedErrorString)
+					t.Logf("                             got %q", err)
+					t.Errorf("unexpected error string")
+				}
+			}
+		})
+
+	}
+}
